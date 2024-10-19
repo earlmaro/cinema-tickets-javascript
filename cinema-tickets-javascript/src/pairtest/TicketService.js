8672e16fe41ce3dd295b3d661287bcb299f50732
preciousagoma <precious@web-epoch.com>
2024-10-19 23:08:54 +0100

    created service to handle ticket purchase

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -5,16 +5,15 @@ export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
-
   #reservations = [];
   purchaseTickets(accountId, ...ticketTypeRequests) {
     try {
       this.#validateAccountId(accountId);
       const totalTickets = this.#calculateTotalTickets(ticketTypeRequests);
+      this.#validateTotalTickets(totalTickets);
+      this.#validateTicketTypes(ticketTypeRequests);
+      this.#createReservations(ticketTypeRequests);
       console.log(totalTickets);
-      // this.#validateTotalTickets(totalTickets);
-      // this.#validateTicketTypes(ticketTypeRequests);
-      // this.#createReservations(ticketTypeRequests);
       //
       // // Here, you might call payment or seat reservation services later
       // return { reservations: this.#reservations, totalTickets };
@@ -32,11 +31,43 @@ export default class TicketService {
   }
 
   #calculateTotalTickets(ticketTypeRequests) {
-    return ticketTypeRequests.reduce((total, request) => total + this.#getNoOfTickets(), 0);
+    return ticketTypeRequests.reduce((total, item) => total + item.noOfTicket, 0);
   }
-
   #getNoOfTickets(ticketTypeRequests) {
     return Object.values(TicketTypes);
   }
+  #validateTotalTickets(totalTickets) {
+    if (totalTickets > 25) {
+      throw new Error('Cannot request more than 25 tickets.');
+    }
+  }
+
+  #validateTicketTypes(ticketTypeRequests) {
+    let containsDependentTicket = false; // Flag for ticket with cannotBeAlone = true
+    let containsIndependentTicket = false; // Flag for ticket with cannotBeAlone = false
+    ticketTypeRequests.forEach(ticket => {
+      const ticketDetails = TicketTypes.find(t => t.slug === ticket.slug);
+      if (!ticketDetails) {
+        throw new InvalidPurchaseException(`Invalid ticket type: ${ticket.slug}`);
+      }
+
+      // Update flags based on cannotBeAlone attribute
+      if (ticketDetails.cannotBeAlone) {
+        containsDependentTicket = true;
+      } else {
+        containsIndependentTicket = true;
+      }
+    })
+    if (containsDependentTicket && !containsIndependentTicket) {
+      throw new InvalidPurchaseException('Tickets that cannot be alone (like infants) must be accompanied by another ticket.');
+    }
+  }
+
+  #createReservations(ticketTypeRequests) {
+    if (totalTickets > 25) {
+      throw new Error('Cannot request more than 25 tickets.');
+    }
+  }
+
 
 }
--- cinema-tickets-javascript/src/pairtest/lib/TicketTypeRequest.js
+++ cinema-tickets-javascript/src/pairtest/lib/TicketTypeRequest.js
@@ -6,11 +6,10 @@ export default class TicketTypeRequest {
   #type;
 
   #noOfTickets;
-
-  static #validTypes = ['ADULT', 'CHILD', 'INFANT']; // Valid ticket types
-
   constructor(type, noOfTickets) {
-    if (!this.TICKET_TYPES.includes(type)) {
+    const ticketType = TicketTypes.find(ticket => ticket.slug === type);
+
+    if (!ticketType) {
       throw new TypeError(`type must be ${this.TICKET_TYPES.slice(0, -1).join(', ')}, or ${this.TICKET_TYPES.slice(-1)}`);
     }
 
