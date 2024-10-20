463d41983a889976e8f5c5da5351774f4605fe3a
preciousagoma <precious@web-epoch.com>
2024-10-20 19:19:04 +0100

    wip* creating error class

--- cinema-tickets-javascript/src/pairtest/lib/InvalidPurchaseException.js
+++ cinema-tickets-javascript/src/pairtest/lib/InvalidPurchaseException.js
@@ -1,3 +1,20 @@
 export default class InvalidPurchaseException extends Error {
+    constructor(message, statusCode = 400, errorCode = 'INVALID_PURCHASE') {
+        // Call the parent class (Error) constructor with the message
+        super(message);
 
+        // Maintain the name of the class as the error name
+        this.name = this.constructor.name; // 'InvalidPurchaseException'
+
+        // Assign a status code (400 by default for a bad request)
+        this.statusCode = statusCode;
+
+        // Assign a custom error code to differentiate error types
+        this.errorCode = errorCode;
+
+        // Capture stack trace for debugging (optional)
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
 }
